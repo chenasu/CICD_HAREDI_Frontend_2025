17101d9899be12470f4cd90706beefa2b639146e
Commit for moving computer <aslcn7@gmail.com>
2025-03-30 16:01:24 +0300

    Send name to server

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 // import reactLogo from './assets/react.svg'
 import './App.css';
-  
+
+const SERVER_URL = "https://cicd-2025-server.onrender.com";
 function App() {
   const [count, setCount] = useState(0); // Learn about Hooks in react.
+  const [firstName, setFirstName] = useState(""); // Learn about Hooks in react.
 
-  function countUp() {
-    console.log('Counting up...');
-    setCount(count => count + 1);
+  function onTextChange(event){
+    setFirstName(event.target.value);
   }
+  
+  useEffect(()=>{
+    console.log(`Hello, ${firstName}!`);
+
+    async function submitName(name){
+      const response = await fetch(SERVER_URL, {
+        body: JSON.stringify({"name": name}),
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })   
+      return response
+    }
+    const res = submitName(firstName)
+    console.log(`response, ${res}!`);
+    
+  }, [firstName])
+  
 
   return (
     <>
@@ -16,18 +36,17 @@ function App() {
         <a href='https://google.com' target='_blank'>
           <img src={'https://www.sce.ac.il/ver/14/tpl/website/img/SamiSH-logo_2.png'} className='logo react' alt='Vite logo' />
         </a>
-        {/* <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
       </div>
       <h1>Welcome to CICD Class with Chen Asulin</h1>
       <div className='card'>
-        <button onClick={countUp}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <input 
+        type='text' 
+        name='firstName' 
+        value={firstName}
+        placeholder="Enter your first name"
+        onChange={onTextChange}
+        />
       </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </>
   );
 }
